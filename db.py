9775cb93b0f0e21981b196564be39529174de8ca
Balaji <sbalaj764@apac.comcast.com>
2025-04-23 15:10:25 +0530

    sql change

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import os
 
 def get_connection():
     return pymysql.connect(
-        host=os.getenv("DB_HOST", "your-rds-endpoint.amazonaws.com"),
-        user=os.getenv("DB_USER", "your-db-username"),
-        password=os.getenv("DB_PASSWORD", "your-secure-password"),
-        database=os.getenv("DB_NAME", "retail_store"),
+        host=os.getenv("DB_HOST", "mydb.c1q2w4gis34j.ap-south-1.rds.amazonaws.com"),
+        user=os.getenv("DB_USER", "admin"),
+        password=os.getenv("DB_PASSWORD", "Sreenithi123"),
+        database=os.getenv("DB_NAME", "mydb"),
         cursorclass=pymysql.cursors.DictCursor
     )
